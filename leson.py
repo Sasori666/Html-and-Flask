d24c2cee93b1fa497804b04a144d147c118bc0f1
Sasori666 <50064989+Sasori666@users.noreply.github.com>
2021-12-09 15:19:56 +0200

    Add files via upload

---
+++ leson.py
@@ -0,0 +1,6 @@
+import sqlite3 as sq
+with sq.connect("saper.db") as con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE score > 100 ORDER BY score DESC LIMIT 5")
+result = cur.fetchall()
+print(result)
